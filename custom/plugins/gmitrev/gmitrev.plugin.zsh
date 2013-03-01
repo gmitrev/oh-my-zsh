dbb74fd1b0ec1ef7ce6d698135f0678c4317ea68
georgi <gvmitrev@gmail.com>
2013-03-01 11:55:11 +0200

    Custom folder added to tree

---
+++ custom/plugins/gmitrev/gmitrev.plugin.zsh
@@ -0,0 +1,14 @@
+alias tlog='tail -f log/development.log'
+alias cdp='cd ~/code/rb/pms'
+
+c() { cd ~/code/$1; }
+_c() { _files -W ~/code -/; }
+compdef _c c
+
+h() { cd ~/$1; }
+_h() { _files -W ~/ -/; }
+compdef _h h
+
+unsetopt correct_all
+
+export PATH="$(dirname $0)/bin:$PATH"
